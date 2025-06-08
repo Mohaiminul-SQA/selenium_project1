9d0ef330adcb57dc55297bd515fabdcbadf1c81c
Eshrak <mirmohaiminul786@gmail.com>
2025-06-08 23:36:12 +0600

    locating with css selector and x-path

--- src/test/java/org/bootcamp/LocatorWebElement.java
+++ src/test/java/org/bootcamp/LocatorWebElement.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class LocatorWebElement extends DriverSetup{
 
     @Test
     public void locate() {
         try {
             driver.get("https://google.com");
-            WebElement searchElement;
+            WebElement searchElement,searchArea,searchArea2;
 //            searchElement= driver.findElement(By.id("APjFqb")); //locate by id
 //            searchElement = driver.findElement(By.name("q")); //locate by name
 //            searchElement = driver.findElement(By.tagName("textarea")); //locate by tag name
@@ -23,6 +25,27 @@ public class LocatorWebElement extends DriverSetup{
             driver.navigate().back();
             driver.findElement(By.partialLinkText("কীভাবে কাজ করে")).click();
             Thread.sleep(3000);
+            driver.navigate().back();
+
+            //xpath expression
+//            searchArea = driver.findElement(By.xpath("//textarea[@class='gLFyf']"));
+//            searchArea.sendKeys("textarea found successfully");
+//            Thread.sleep(3000);
+//            searchArea.clear();
+//            Thread.sleep(3000);
+
+            //css selector
+//            searchArea2 = driver.findElement(By.cssSelector("textarea[class='gLFyf']"));
+//            searchArea2.sendKeys("textarea found again successfully");
+//            Thread.sleep(3000);
+
+            //locate child element
+            List<WebElement> searchElements = driver.findElements(By.xpath("//div[@class='KxwPGc AghGtd']/a"));
+            for (WebElement element : searchElements) {
+                System.out.println(element.getText());
+            }
+
+
         }catch(Exception e) {
             System.out.println(e.getMessage());
         }
